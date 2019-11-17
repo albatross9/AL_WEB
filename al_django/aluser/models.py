4129aa146ebbd27c6b9fb7bbfc76ead6fc3aa3b7
Albatross9 <albatross918@daum.net>
2019-11-18 00:47:16 +0900

    모델설정

--- al_django/aluser/models.py
+++ al_django/aluser/models.py
@@ -4,3 +4,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Aluser(models.Model):
+    email = models.EmailField(verbose_name='이메일')
+    password = models.CharField(max_length=64, verbose_name="비번")
+    register_date = models.DateTimeField(auto_now=True, verbose_name="등록일")
+
+    class Meta:
+        db_table = 'albatross9_aluser'
+        verbose_name = '사용자'
+        verbose_name_plural = '사용자'
--- al_django/order/models.py
+++ al_django/order/models.py
@@ -4,3 +4,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Order(models.Model):
+    aluser = models.ForeignKey(
+        'aluser.Aluser', on_delete=models.CASCADE, verbose_name='사용자')
+    product = models.ForeignKey(
+        'product.Product', on_delete=models.CASCADE, verbose_name="상품")
+    register_date = models.DateTimeField(auto_now=True, verbose_name='등록날짜')
+    quantity = models.IntegerField(verbose_name='수량')
+
+    class Meta:
+        db_table = 'albatross9_order'
+        verbose_name = '주문'
+        verbose_name_plural = '주문'
